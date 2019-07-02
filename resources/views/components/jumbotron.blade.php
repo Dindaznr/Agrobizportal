179b7c671b04ef612458dbb08262a0de36508669
Dindaznr <rahmannisa20@gmail.com>
2019-07-02 08:44:34 +0700

    [UPDATE] update asset data products

--- resources/views/components/jumbotron.blade.php
+++ resources/views/components/jumbotron.blade.php
@@ -3,7 +3,7 @@
         <h1 class="jumbotron-heading">Agrobizportal E-Commerce</h1>
         <p class="lead text-muted mb-0">
             Agrobizportal adalah website-platform pertama di Budi Luhur dan
-            Sekitarnya yang menawarkan transaksi jual beli online yang menyenangkan, gratis, dan terpercaya via website.
+            Sekitarnya yang menawarkan transaksi jual beli online yang menyenangkan, dan terpercaya via website.
             Bergabunglah dengan jutaan pengguna lainnya dengan mulai mendaftarkan produk jualan dan berbelanja berbagai
             penawaran menarik kapan saja, di mana saja.
             Ayo gabung dalam komunitas Agrobizportal dan mulai belanja sekarang!
